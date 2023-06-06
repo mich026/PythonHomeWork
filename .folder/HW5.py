ef005c86fc9cecffe06c802a1c8ed55a34ec87ff
MariannaChernova <mich026@yandex.ru>
2023-06-06 14:41:40 +0300

    Задачи 26,28

---
+++ .folder/HW5.py
@@ -0,0 +1,27 @@
+# Задача 26.
+# a = int(input("Введите число "))
+# b = int(input("Введите степень числа(целое неотрицательно число) "))
+
+
+# def func(a, b):
+#     if b == 0:
+#         return 1
+
+#     return a * func(a, b - 1)
+
+
+# print(func(a, b))
+
+# Задача 28.
+# a = int(input("Введите первое неотрицительное число "))
+# b = int(input("Введите второе неотрицательно число "))
+
+
+# def recursive_sum(a, b):
+#     if a == 0:
+#         return b
+#     else:
+#         return recursive_sum(a - 1, b + 1)
+
+
+# print(recursive_sum(a, b))
